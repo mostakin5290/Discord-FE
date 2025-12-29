a1c7b3fa5e142b4f0a1f491f87ee00096c6ad853
Mehul Prajapati <ailearn90@gmail.com>
2025-12-30 03:37:22 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button className="text-red-400 bg-neutral-900">
+        Hello World
+      </Button>
+    </div>
+  )
+}
+
+export default App;
